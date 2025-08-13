b17f65210cc6a7f2049be3ccdd5337590d85563a
Sothi Khorn <sothikhorn@Sothis-MacBook-Pro.local>
2025-08-13 21:27:37 +0700

    remove log files

--- config.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import torch
-from threadpoolctl import threadpool_info
-
-print(numpy.show_config())
-print("------------------------")
-print(torch.__config__.show())
-print("------------------------")
-print(threadpool_info())
